248e97cf1a508f1b9c81f67866188bf9e1ad204c
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2013-01-17 01:02:00 +0400

    Deltas moved into separated array

--- fuzzy_car.rb
+++ fuzzy_car.rb
@@ -63,15 +63,34 @@ end
 class SlowdownAlgorithm
   attr_accessor :car
   attr_accessor :rules
+  attr_accessor :deltas
   attr_accessor :strategy
+
+  def self.create(deltas)
+    SlowdownAlgorithm.new.tap do |algorithm|
+      algorithm.car = Car.new(2000.0, 80.0, 990.0)
+      algorithm.rules = [
+        { :speed => Accessory.create(:speed, :low), :distance => Accessory.create(:distance, :small) },
+        { :speed => Accessory.create(:speed, :low), :distance => Accessory.create(:distance, :average) },
+        { :speed => Accessory.create(:speed, :low), :distance => Accessory.create(:distance, :large) },
+        { :speed => Accessory.create(:speed, :average), :distance => Accessory.create(:distance, :small) },
+        { :speed => Accessory.create(:speed, :average), :distance => Accessory.create(:distance, :average) },
+        { :speed => Accessory.create(:speed, :average), :distance => Accessory.create(:distance, :large) },
+        { :speed => Accessory.create(:speed, :high), :distance => Accessory.create(:distance, :small) },
+        { :speed => Accessory.create(:speed, :high), :distance => Accessory.create(:distance, :average) },
+        { :speed => Accessory.create(:speed, :high), :distance => Accessory.create(:distance, :large) }
+      ]
+      algorithm.deltas = deltas.map{|d| d * MAX_DELTA}
+    end
+  end
 
   def energy
     pw_sum = 0.0
     w_sum = 0.0
-    rules.each do |rule|
+    rules.each_with_index do |rule, i|
       w = rule[:speed].value(car.speed) * rule[:distance].value(car.distance)
       w_sum += w
-      pw_sum += w * rule[:delta]
+      pw_sum += w * deltas[i]
     end
     pw_sum / w_sum
   end
@@ -87,56 +106,9 @@ end
 
 # value of delta for rules was obtained experimentally
 
-
-algorithm = SlowdownAlgorithm.new.tap do |algorithm|
-  algorithm.car = Car.new(2000.0, 80.0, 990.0)
-  algorithm.rules = [
-    {
-      :speed => Accessory.create(:speed, :low), 
-      :distance => Accessory.create(:distance, :small),
-      :delta => 0.9579651951789856 * MAX_DELTA
-    },
-    {
-      :speed => Accessory.create(:speed, :low), 
-      :distance => Accessory.create(:distance, :average),
-      :delta => 0.28362135149708406 * MAX_DELTA
-    },
-    {
-      :speed => Accessory.create(:speed, :low), 
-      :distance => Accessory.create(:distance, :large),
-      :delta => 0.175209947623089 * MAX_DELTA
-    },
-    {
-      :speed => Accessory.create(:speed, :average), 
-      :distance => Accessory.create(:distance, :small),
-      :delta => 0.9522131839125745 * MAX_DELTA
-    },
-    {
-      :speed => Accessory.create(:speed, :average), 
-      :distance => Accessory.create(:distance, :average),
-      :delta => 0.9787151217460632 * MAX_DELTA
-    },
-    {
-      :speed => Accessory.create(:speed, :average), 
-      :distance => Accessory.create(:distance, :large),
-      :delta => 0.2869786322116852 * MAX_DELTA
-    },
-    {
-      :speed => Accessory.create(:speed, :high), 
-      :distance => Accessory.create(:distance, :small),
-      :delta => 0.18289752650522906 * MAX_DELTA
-    },
-    {
-      :speed => Accessory.create(:speed, :high), 
-      :distance => Accessory.create(:distance, :average),
-      :delta => 0.8402485431520775 * MAX_DELTA
-    },
-    {
-      :speed => Accessory.create(:speed, :high), 
-      :distance => Accessory.create(:distance, :large),
-      :delta => 0.1791947278657684 * MAX_DELTA
-    },
-  ]
-end
-
-algorithm.stop_the_car
+deltas = [
+  0.9579651951789856, 0.28362135149708406, 0.175209947623089, 
+  0.9522131839125745, 0.9787151217460632, 0.2869786322116852,
+  0.18289752650522906, 0.8402485431520775, 0.1791947278657684 
+]
+SlowdownAlgorithm.create(deltas).stop_the_car
